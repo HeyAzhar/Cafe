0127651aeaeeb48bc1138e79e6ab3722234e9796
HeyAzhar <sk.azharuddin1230@gmail.com>
2020-05-06 05:36:43 +0530

    Location Section Added

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ var options = {
   scrollPollInterval: 40
 }
 $('.aniview').AniView(options);
+
+function initMap() {
+  var cafe = {lat: 12.9159, lng: 77.60556};
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: cafe});
+  var marker = new google.maps.Marker({position: cafe, map: map});
+}
